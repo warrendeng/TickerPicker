85ce2afe060c4c0ce147d72081bd5feb973c5bd3
Warren Deng <wdeng@berkeley.edu>
2019-10-26 20:19:35 -0700

    finished sentiment score and magnitude

--- stockTwitter.py
+++ stockTwitter.py
@@ -5,8 +5,13 @@ from gs_quant.session import GsSession
 from twython import Twython 
 import json
 import sys
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+# Instantiates a client
+client = language.LanguageServiceClient()
 
-
+# The text to analyze
 twitter = Twython('oGKKKOtLYiIOazQSKc1ciMu4q', 'fsZe6SGCi8CY6dK1vEniFK3eZr0z17KiL0ncLxBEx2aw2v4RGL')
 
 
@@ -17,10 +22,22 @@ GsSession.use(client_id = client_id, client_secret = client_secret, scopes = sco
 print("Enter a list of stock tickers separated by comma:")
 stocks = input()
 stockList = stocks.split(', ')
-dict_ = {'text': []}
+sentimentDict = {}
 for stock in stockList:
 	query = {'q': stock, 'result_type': 'popular', 'count': 100, 'lang': 'en'}
-	dict_[stock] = []
+	sentimentScore = sentimentMagnitude = ctr = 0
 	for status in twitter.search(**query)['statuses']:
-		dict_[stock].append(status['text'])
-print(dict_)
+		text = status['text']
+		document = types.Document(
+		    content=text,
+		    type=enums.Document.Type.PLAIN_TEXT)
+
+		# Detects the sentiment of the text
+		sentiment = client.analyze_sentiment(document=document).document_sentiment
+		sentimentScore += sentiment.score
+		sentimentMagnitude += sentiment.magnitude
+		ctr += 1
+	sentimentDict[stock] = [sentimentScore / ctr, sentimentMagnitude / ctr]
+print(sentimentDict)
+		#print('Text: {}'.format(text))
+		#print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
