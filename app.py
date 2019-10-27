c643080bd594eb36598d8494f0c0bc5887904df2
Warren Deng <wdeng@berkeley.edu>
2019-10-27 02:54:42 -0700

    daniel fix ur nn:

--- app.py
+++ app.py
@@ -24,7 +24,20 @@ def queryList():
     else:
         return "bad"
 
-
+@app.route('/sentiNN', methods=['GET'])
+def queryList():
+    if request.method == 'GET':
+        tickerList = request.get_json()["data"]
+        tickerListMap = [getSentiment(s) for s in tickerList]
+        classificationMap = [danielMethod(s, tickerListMap[s]) for s in tickerListMap]
+        response = app.response_class(
+          response=json.dumps(tickerListMap),
+          status=200,
+          mimetype='application/json'
+        )
+        return response
+    else:
+        return "bad"
 
 @app.route('/singleSentiment/<ticker>')
 def querySingle(ticker=None):
